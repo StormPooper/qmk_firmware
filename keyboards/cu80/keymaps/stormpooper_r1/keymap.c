9ce385c9de425981d54377ff4cf2231bf020bf85
Daniel Smith <danny@stormpoopersmith.com>
2020-08-04 12:44:51 +0100

    Remove Tap Dance, just Key Lock

--- keyboards/cu80/keymaps/stormpooper_r1/keymap.c
+++ keyboards/cu80/keymaps/stormpooper_r1/keymap.c
@@ -20,15 +20,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define _BASE 0
 #define _HOTKEYS 1
 
-enum {
-    TD_LOCK_CAPS,
-};
-
-qk_tap_dance_action_t tap_dance_actions[] = {
-    // Tap once for Key Lock, twice for Caps Lock
-    [TD_LOCK_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_LOCK, KC_CAPS),
-};
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE]    = LAYOUT_all(KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_PSCR, KC_SLCK, KC_PAUS,
 
@@ -36,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
                          KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_DEL, KC_END, KC_PGDN,
 
-                         TD(TD_LOCK_CAPS), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT,
+                         KC_LOCK, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT,
 
                          KC_LSFT, KC_NUBS, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP,
 
